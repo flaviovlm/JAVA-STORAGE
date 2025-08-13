f4ddf5a6fea66d256d8c12a4c3ff376a34a83b24
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-13 20:39:14 -0300

    feat:  Trabalhando com vetores.

---
+++ Vetores/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Double> notas = new ArrayList<>();
+
+        notas.add (10.0);
+        notas.add(9.0);
+
+        for (double nota: notas){
+            System.out.println("Nota: "+nota);
+        }
+
+    }
+}
