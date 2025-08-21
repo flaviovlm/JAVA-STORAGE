cb3292bdf76db1168ea9b4d07396c84e4e4a23f5
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-20 21:16:48 -0300

    feat: Trabalhando com encapsulamento

---
+++ Encapsulamento/src/main/java/atividade_2/Cliente.java
@@ -0,0 +1,67 @@
+package atividade_2;
+
+public class Cliente {
+    private String nome;
+    private int idade;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+
+    public Cliente(String nome, int idade, String cpf, String endereco, String telefone) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente " +
+                "\nNome = " + nome +
+                "\nIdade = " + idade +
+                "\nCpf = " + cpf +
+                "\nEndereco = " + endereco +
+                "\nTelefone = " + telefone;
+    }
+}
--- Encapsulamento/src/main/java/atividade_2/Main.java
+++ Encapsulamento/src/main/java/atividade_2/Main.java
@@ -7,10 +7,12 @@ public class Main {
 
         Veiculo veiculo1 = new Veiculo("FFFF-6666", "Cinza", 50,8,276,36.7);
         Livro livro1 = new Livro("Minha Luta", "Adolf Guilherme", "978-85-12345-67-8",700, 79.99);
-
+        Cliente cliente1 = new Cliente("Flávio", 20, "111.222.333-44","Rua alemanha", "71981339100");
         System.out.println(veiculo1.toString());
         System.out.println();
         System.out.println(livro1.toString());
+        System.out.println();
+        System.out.println(cliente1.toString());
     }
 
 }
